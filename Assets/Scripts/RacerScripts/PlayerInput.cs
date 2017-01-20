5204b1a5fdf8bf2b059038fbd1290828906329cb
Jmeas <jellyes2@gmail.com>
2017-01-19 20:01:55 -0800

    Add files to repository

---
+++ Assets/Scripts/RacerScripts/PlayerInput.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+// This controls a "Racer" object through player input. It's attached
+// to the GameObject that displays the player's character.
+public class PlayerInput : UnityEngine.MonoBehaviour {
+  public GameObject storeObject;
+  protected Store store;
+  protected Racer racer;
+
+  // How fast the player's car goes
+  Vector3 velocity;
+
+  // How long, in milliseconds, it takes this car to change lanes
+  //  float changeLaneTime = 500;
+
+  void Start() {
+    this.store = storeObject.GetComponent<Store>();
+    this.racer = this.store.player;
+
+    this.velocity = new Vector3(this.racer.speed, 0, 0);
+  }
+
+  void Update() {
+    this.CheckLaneChange();
+    transform.position += velocity * Time.deltaTime;
+  }
+
+  // Determine if we need to begin a lane transition, based on key presses
+  void CheckLaneChange() {
+    if (Input.GetKeyDown(KeyCode.UpArrow)) {
+      if (this.racer.ChangeLanes("up")) {
+        this.ChangeLane();
+      }
+    } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+      if (this.racer.ChangeLanes("down")) {
+        this.ChangeLane();
+      }
+    }
+  }
+
+  // Actually perform a lane change.
+  // TODO: Make this occur over time, using the `changeLaneTime`. Right now,
+  // this occurs instantaneously
+  void ChangeLane() {
+    this.racer.CompleteChangeLanes();
+    float newY = Lanes.firstLaneYPosition + (this.racer.lane * Lanes.Height);
+    this.transform.position = new Vector3(this.transform.position.x, newY, this.transform.position.z);
+  }
+}
---
+++ Assets/Scripts/State/Init.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This instantiates some of the data that populates the store.
+// Right now, this is done on Awake(), but we will probably want to
+// adjust that once there's, for instance, a Main Menu.
+public class Init : UnityEngine.MonoBehaviour {
+  public Store store;
+  public GameObject playerGameObject;
+
+  // TODO: Do I really want to do this doing Awake? I'm currently doing
+  // it to ensure that the store is configured before anything else's "Start()"
+  // script is called. I may need to think about the flow of things when the user
+  // clicks "New Game," which would kick off these things.
+  void Awake() {
+    store = this.gameObject.GetComponent<Store>();
+    playerGameObject = GameObject.Find("Car");
+
+    this.CreateNewGame();
+  }
+
+  void CreateNewGame() {
+    // Create our Player object
+    // First, we need to get the player's width from its sprite.
+    SpriteRenderer playerRenderer = playerGameObject.GetComponent<SpriteRenderer>();
+    float width = playerRenderer.sprite.rect.size.x;
+    Racer player = new Racer(
+      name: "Player",
+      speed: 40f,
+      store: store,
+      width: width
+    );
+
+    // Add the player to the store, directly and in the `racers` array.
+    store.player = player;
+    store.racers.Add(player);
+
+    // TODO: Create the 7 other racers
+    // Also create anything else that we may need.
+  }
+}
